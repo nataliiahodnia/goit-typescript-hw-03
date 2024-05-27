d0cccad603bb7f1a6334ec540056ffe75abe0520
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-05-27 10:58:20 +0200

    try+

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,67 @@
-const key = new Key();
+// const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+// const house = new MyHouse(key);
+// const person = new Person(key);
 
-house.openDoor(person.getKey());
+// house.openDoor(person.getKey());
 
-house.comeIn(person);
+// house.comeIn(person);
 
 
-export {};
+// export {};
+
+class Key {
+     private signature: number = Math.floor(Math.random() * 100) + 1;
+   
+     getSignature(): number {
+       return this.signature;
+     }
+   }
+   
+   class Person {
+     constructor(private key: Key) {}
+   
+     getKey(): Key {
+       return this.key;
+     }
+   }
+   
+   abstract class House {
+     public door: boolean = false;
+     public tenants: Person[] = [];
+   
+     constructor(public key: Key) {}
+   
+     comeIn(person: Person): void {
+       if (this.door) {
+         this.tenants.push(person);
+         console.log("Person came in.");
+       } else {
+         console.log("Door is closed.");
+       }
+     }
+   
+     abstract openDoor(key: Key): void;
+   }
+   
+   class MyHouse extends House {
+     openDoor(key: Key): void {
+       if (key.getSignature() === this.key.getSignature()) {
+         this.door = true;
+         console.log("Door opened.");
+       } else {
+         console.log("Invalid key.");
+       }
+     }
+   }
+   
+   const key = new Key();
+   
+   const house = new MyHouse(key);
+   const person = new Person(key);
+   
+   house.openDoor(person.getKey());
+   
+   house.comeIn(person);
+   
+   export {};
